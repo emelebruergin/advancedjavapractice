578d50451030e78ff290d93481fd704d1554ece8
emelebruergin <e.ebruergin@hotmail.com>
2023-05-20 16:01:19 +0300

    odev

--- src/main/java/advancedJavaPrc/multithreads/threadpool/AppointmentApp2.java
+++ src/main/java/advancedJavaPrc/multithreads/threadpool/AppointmentApp2.java
@@ -1,4 +1,8 @@
 package advancedJavaPrc.multithreads.threadpool;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /*
 Thread oluşturmak maliyetli bir olaydır.Her thread için sistemden belli bir kaynak ayırılır.
 Bu kaynaklar CPU, Hafıza gibi önemli olanlardır. Uygulamamız çalışırken belli bir miktarda
@@ -12,4 +16,31 @@ Uygulama herbir talep için bir gün sonrasını tarih vermelidir. 5 kişi için
 randevu alınmak isteniyor. Thread havuzu oluşturup işleri 3 threade yaptırınız
  */
 public class AppointmentApp2 {
+    public static void main(String[] args) {
+        ExecutorService service= Executors.newFixedThreadPool(3);
+        String[] users = {"Bill", "Steve", "Linux", "Tim", "Larry"};
+        AppointmentCenter2 appointmentCenter=new AppointmentCenter2();
+        for (String user:users){
+            MyThread thread=new MyThread(appointmentCenter);
+            service.execute(thread);
+        }
+service.shutdown();
+    }
+}
+
+class MyThread extends Thread{
+    private AppointmentCenter2 appointmentCenter;
+
+    public MyThread(AppointmentCenter2 appointmentCenter) {
+        this.appointmentCenter = appointmentCenter;
+    }
+
+    @Override
+    public void run() {
+        String name=Thread.currentThread().getName();
+        System.out.println("++"+name+" başladı");
+        System.out.println("Randevu tarihiniz"+appointmentCenter.getAppointmentDate());
+        System.out.println("---"+name+" bitti");
+
+    }
 }
